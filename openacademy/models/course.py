e716239ced0e8afda2679befa0f83fdde74c66b3
abk16 <and.theunnamed@gmail.com>
2021-03-16 19:59:53 +0100

    [IMP] Black formatted

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -3,29 +3,37 @@ from odoo import fields, models
 
 
 class Course(models.Model):
-    _name = 'openacademy.course'
-    _description = 'Course'
+    _name = "openacademy.course"
+    _description = "Course"
 
-    name = fields.Char(string='Title', required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
-    responsible_id = fields.Many2one('openacademy.partner', string="Responsible")
-    session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions")
+    responsible_id = fields.Many2one("openacademy.partner", string="Responsible")
+    session_ids = fields.One2many("openacademy.session", "course_id", string="Sessions")
 
-    level = fields.Selection([('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')], string="Difficulty Level")
+    level = fields.Selection(
+        [("easy", "Easy"), ("medium", "Medium"), ("hard", "Hard")],
+        string="Difficulty Level",
+    )
 
 
 class Session(models.Model):
-    _name = 'openacademy.session'
-    _description = 'Session'
+    _name = "openacademy.session"
+    _description = "Session"
 
     name = fields.Char(required=True)
     active = fields.Boolean(default=True)
-    state = fields.Selection([('draft', "Draft"), ('confirmed', "Confirmed"), ('done', "Done")], default='draft')
+    state = fields.Selection(
+        [("draft", "Draft"), ("confirmed", "Confirmed"), ("done", "Done")],
+        default="draft",
+    )
 
     start_date = fields.Date(default=fields.Date.context_today)
     duration = fields.Float(digits=(6, 2), help="Duration in days", default=1)
 
-    instructor_id = fields.Many2one('openacademy.partner', string="Instructor")
-    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('openacademy.partner', string="Attendees")
+    instructor_id = fields.Many2one("openacademy.partner", string="Instructor")
+    course_id = fields.Many2one(
+        "openacademy.course", ondelete="cascade", string="Course", required=True
+    )
+    attendee_ids = fields.Many2many("openacademy.partner", string="Attendees")
