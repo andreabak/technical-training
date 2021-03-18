5b9be71d7abb69f1f2571d6bb357d0ce819a444c
abk16 <and.theunnamed@gmail.com>
2021-03-18 11:33:47 +0100

    [SOLUTION] 04 - Model Inheritance: P2 Library

--- library/models/book.py
+++ library/models/book.py
@@ -13,4 +13,18 @@ class Books(models.Model):
     isbn = fields.Char(string="ISBN")
     publisher_id = fields.Many2one("library.publisher", string="Publisher")
 
-    rental_ids = fields.One2many("library.rental", "book_id", string="Rentals")
+    # TODO: can we show all rented book copies from the book itself
+
+
+class BookCopy(models.Model):
+    _name = "library.book.copy"
+    _description = "Book Copy"
+
+    _inherits = {"library.book": "book_id"}
+
+    book_id = fields.Many2one("library.book", "Book", required=True, ondelete="cascade")
+
+    reference = fields.Char("Reference", required=True, index=True)
+    rental_id = fields.One2many(
+        "library.rental", "book_copy_id", string="Rentals", readonly=True
+    )
--- library/models/rental.py
+++ library/models/rental.py
@@ -6,8 +6,11 @@ class Rentals(models.Model):
     _name = "library.rental"
     _description = "Book rental"
 
-    customer_id = fields.Many2one("library.partner", string="Customer")
-    book_id = fields.Many2one("library.book", string="Book")
+    customer_id = fields.Many2one("library.partner", string="Customer", required=True)
+    book_copy_id = fields.Many2one(
+        "library.book.copy", string="Book Copy", required=True
+    )
+    # TODO: Unique constraints for book copies not returned
 
     rental_date = fields.Date()
     return_date = fields.Date()
@@ -15,6 +18,6 @@ class Rentals(models.Model):
     customer_address = fields.Text(related="customer_id.address")
     customer_email = fields.Char(related="customer_id.email")
 
-    book_authors = fields.Many2many(related="book_id.author_ids")
-    book_edition_date = fields.Date(related="book_id.edition_date")
-    book_publisher = fields.Many2one(related="book_id.publisher_id")
+    book_authors = fields.Many2many(related="book_copy_id.author_ids")
+    book_edition_date = fields.Date(related="book_copy_id.edition_date")
+    book_publisher = fields.Many2one(related="book_copy_id.publisher_id")
