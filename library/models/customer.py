1d9ae9b32fd4777cea846b9482043bb5c5692563
abk16 <and.theunnamed@gmail.com>
2021-03-23 14:15:31 +0100

    [IMP] applied pre-commit stuff

--- library/models/customer.py
+++ library/models/customer.py
@@ -1,39 +1,58 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    author = fields.Boolean('is an Author', default=False)
-    publisher = fields.Boolean('is a Publisher', default=False)
+    author = fields.Boolean("is an Author", default=False)
+    publisher = fields.Boolean("is a Publisher", default=False)
 
-    current_rental_ids = fields.One2many('library.rental', 'customer_id', string='Current Rentals', domain=[('state', '=', 'rented')])
-    old_rental_ids = fields.One2many('library.rental', 'customer_id', string='Old Rentals', domain=[('state', '=', 'returned')])
-    lost_rental_ids = fields.One2many('library.rental', 'customer_id', string='Lost Rentals', domain=[('state', '=', 'lost')])
+    current_rental_ids = fields.One2many(
+        "library.rental",
+        "customer_id",
+        string="Current Rentals",
+        domain=[("state", "=", "rented")],
+    )
+    old_rental_ids = fields.One2many(
+        "library.rental",
+        "customer_id",
+        string="Old Rentals",
+        domain=[("state", "=", "returned")],
+    )
+    lost_rental_ids = fields.One2many(
+        "library.rental",
+        "customer_id",
+        string="Lost Rentals",
+        domain=[("state", "=", "lost")],
+    )
 
-    book_ids = fields.Many2many("product.product", string="Books", domain=[('book', '=', True)])
+    book_ids = fields.Many2many(
+        "product.product", string="Books", domain=[("book", "=", True)]
+    )
     copy_ids = fields.Many2many("library.copy", string="Book Copies")
-    nationality_id = fields.Many2one('res.country', 'Nationality')
-    birthdate = fields.Date('Birthdate')
+    nationality_id = fields.Many2one("res.country", "Nationality")
+    birthdate = fields.Date("Birthdate")
 
-    qty_lost_book = fields.Integer('Number of book copies lost', compute="_get_lost_books_qty")
-    payment_ids = fields.One2many('library.payment', 'customer_id', string='Payments')
-    amount_owed = fields.Float('Amount owed', compute="_amount_owed", store=True)
+    qty_lost_book = fields.Integer(
+        "Number of book copies lost", compute="_get_lost_books_qty"
+    )
+    payment_ids = fields.One2many("library.payment", "customer_id", string="Payments")
+    amount_owed = fields.Float("Amount owed", compute="_amount_owed", store=True)
 
     def _get_lost_books_qty(self):
         for rec in self:
             rec.qty_lost_book = len(rec.lost_rental_ids)
 
-    @api.depends('payment_ids.amount')
+    @api.depends("payment_ids.amount")
     def _amount_owed(self):
         for rec in self:
-            rec.amount_owed = - sum(rec.payment_ids.mapped('amount'))
+            rec.amount_owed = -sum(rec.payment_ids.mapped("amount"))
 
 
 class Payment(models.Model):
-    _name = 'library.payment'
-    _description = 'Payment'
+    _name = "library.payment"
+    _description = "Payment"
 
     date = fields.Date(required=True, default=fields.Date.context_today)
     amount = fields.Float()
